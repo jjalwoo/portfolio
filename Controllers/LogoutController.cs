a245fed207f44a71bd831f3317f468f56ff7bdda
jjalwoo <0920choi@naver.com>
2024-05-15 18:15:25 +0900

    20240515 다시 올리기

---
+++ Controllers/LogoutController.cs
@@ -0,0 +1,42 @@
+﻿using LoginServer.DB;
+using LoginServer.ErrorCodeEnum;
+using LoginServer.Packet;
+using Microsoft.AspNetCore.Mvc;
+using LoginServer.Redis;
+
+namespace LoginServer.Controllers
+{
+    [Route("[controller]")]
+    public class LogoutController : ControllerBase
+    {
+        IDBRepository _mySqlRepository;
+        private readonly Redis.Redis _redis;
+
+        public LogoutController(IDBRepository mySqlRepository, Redis.Redis redis)
+        {
+            _mySqlRepository = mySqlRepository;
+            _redis = redis;
+        }
+
+        [HttpPost]
+        public async Task<ErrorCode> PostAsync([FromBody] LogoutRequest logoutRequest)
+        {
+            LogoutResponse logoutResponse = new();
+
+            // var result = await _mySqlRepository.CheckLoging(logoutRequest.UserID!);
+            var result = _redis.GetValue(logoutRequest.UserID!);
+
+            if (result == null)
+            {
+                logoutResponse.ErrorCode = ErrorCode.Loging;
+                return logoutResponse.ErrorCode;
+            }
+
+            logoutResponse.ErrorCode = ErrorCode.LogoutSuccess;
+            await _mySqlRepository.DeleteToken(logoutRequest.UserID!);
+            
+            _redis.RemoveKey(logoutRequest.UserID!);
+            return logoutResponse.ErrorCode;           
+        }
+    }
+}
---
+++ Redis/Redis.cs
@@ -0,0 +1,53 @@
+﻿using StackExchange.Redis;
+
+namespace LoginServer.Redis
+{
+    public class Redis
+    {
+        ConnectionMultiplexer _redisConn;
+        IDatabase _redisDb;
+
+        public Redis()
+        {
+            _redisConn = ConnectionMultiplexer.Connect("localhost");
+            _redisDb = _redisConn.GetDatabase();
+        }
+
+        public void SetKeyValue(string key, string value)
+        {
+            _redisDb.StringSet(key, value);
+        }
+
+        public string? GetValue(string key)
+        {
+            return _redisDb.StringGet(key);
+        }
+
+        public void RemoveKey(string key)
+        {
+            _redisDb.KeyDelete(key);
+        }
+
+        // 3 !! 
+        
+        // 
+
+        //test 
+        // 
+        public async Task<bool> StoreAccount(string userID)
+        {
+            // redis ! 
+            try
+            {
+                var result = await _redisDb.SetAddAsync("sdf", "sdf");
+                
+            }
+            catch (Exception ex) 
+            {
+                
+            }
+           
+        }
+
+    }
+}
